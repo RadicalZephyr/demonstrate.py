031b93a87fd667f37b6b05e86ca280aeb981e2fa
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-12 10:43:13 -0500

    Add some main and options processing

--- demonstrate.py
+++ demonstrate.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-print("Hello World!")
+import sys, argparse
+import pty
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('script')
+    parser.add_argument('command')
+
+    options = parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
