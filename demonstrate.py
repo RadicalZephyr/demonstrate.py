4192b442a7734dae89b0e5106241f3e14117b2c2
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-06 22:41:24 -0800

    Move all the logic and variables into the class

--- demonstrate.py
+++ demonstrate.py
@@ -5,73 +5,65 @@ import random
 import mypty
 
 
-class HackerReader:
-
-    def __init__(self):
-        pass
-
-    def read(self, stdin):
-        data = os.read(stdin, 1024)
-        return data
-
-
 # Generator works with "with"
 def readgen(fname):
     with open(fname, "r") as f:
         for line in f:
             yield line
 
-# This would be a class, because making a closure like this is sort of
-# awkward in Python.  I said "would" because as it turns out, when I
-# try to pass a class or class function as the function
+class HackerReader:
+
+    def __init__(self, fname):
+        self.fname = fname
+        self.scriptgen = readgen(fname)
+        self.current_line = None
+        self.end_of_file = False
+        self.waiting_for_enter = False
+
+    def read(self, stdin):
+        data = os.read(stdin, 1024)
+
+        # This works to identify an "enter", but will probably not be portable
+        # at all!!!
+        if (waiting_for_enter):
+            if (data == b"\x0D"):
+                waiting_for_enter = False
+                return data
+            else:
+                return None
+
+        if (current_line == None):
+            try:
+                # Try to skip blank lines
+                while not current_line:
+                    current_line = bytes(next(scriptgen)[:-1], "utf-8")
+            except StopIteration:
+                end_of_file = True
+
+        if (not end_of_file):
+            x = len(data) + random.randint(0, 3)
+            ret_data = current_line[:x]
+            current_line = current_line[x:]
+            data = ret_data
+
+            if not data:
+                data = None
+
+            # Reset the line for the next iteration
+            if (len(current_line) == 0):
+                current_line = None
+                waiting_for_enter = True
+
+        return data
+
 def make_reader(file):
 
-    reader = HackerReader()
-    # scriptgen = readgen(file)
-    # current_line = None
-    # end_of_file = False
-    # waiting_for_enter = False
+    reader = HackerReader(file)
 
     def read_file_or_input(stdin):
-        # nonlocal scriptgen
-        # nonlocal current_line
-        # nonlocal end_of_file
-        # nonlocal waiting_for_enter
         nonlocal reader
         return reader.read(stdin)
 
-        # # # This works to identify an "enter", but will probably not be portable
-        # # # at all!!!
-        # if (waiting_for_enter):
-        #     if (data == b"\x0D"):
-        #         waiting_for_enter = False
-        #         return data
-        #     else:
-        #         return None
-
-        # if (current_line == None):
-        #     try:
-        #         # Try to skip blank lines
-        #         while not current_line:
-        #             current_line = bytes(next(scriptgen)[:-1], "utf-8")
-        #     except StopIteration:
-        #         end_of_file = True
-
-        # if (not end_of_file):
-        #     x = len(data) + random.randint(0, 3)
-        #     ret_data = current_line[:x]
-        #     current_line = current_line[x:]
-        #     data = ret_data
-
-        #     if not data:
-        #         data = None
-
-        #     # Reset the line for the next iteration
-        #     if (len(current_line) == 0):
-        #         current_line = None
-        #         waiting_for_enter = True
-
-        # return data
 
     return read_file_or_input
 
