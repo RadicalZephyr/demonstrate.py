547401fe311dc5e9927e82195ffadf33c820edd6
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-06 22:38:46 -0800

    Get a very basic class reader working

--- demonstrate.py
+++ demonstrate.py
@@ -4,6 +4,17 @@ import sys, os, argparse
 import random
 import mypty
 
+
+class HackerReader:
+
+    def __init__(self):
+        pass
+
+    def read(self, stdin):
+        data = os.read(stdin, 1024)
+        return data
+
+
 # Generator works with "with"
 def readgen(fname):
     with open(fname, "r") as f:
@@ -15,51 +26,52 @@ def readgen(fname):
 # try to pass a class or class function as the function
 def make_reader(file):
 
-    scriptgen = readgen(file)
-    current_line = None
-    end_of_file = False
-    waiting_for_enter = False
+    reader = HackerReader()
+    # scriptgen = readgen(file)
+    # current_line = None
+    # end_of_file = False
+    # waiting_for_enter = False
 
     def read_file_or_input(stdin):
-        nonlocal scriptgen
-        nonlocal current_line
-        nonlocal end_of_file
-        nonlocal waiting_for_enter
+        # nonlocal scriptgen
+        # nonlocal current_line
+        # nonlocal end_of_file
+        # nonlocal waiting_for_enter
+        nonlocal reader
+        return reader.read(stdin)
 
-        data = os.read(stdin, 1024)
+        # # # This works to identify an "enter", but will probably not be portable
+        # # # at all!!!
+        # if (waiting_for_enter):
+        #     if (data == b"\x0D"):
+        #         waiting_for_enter = False
+        #         return data
+        #     else:
+        #         return None
 
-        # # This works to identify an "enter", but will probably not be portable
-        # # at all!!!
-        if (waiting_for_enter):
-            if (data == b"\x0D"):
-                waiting_for_enter = False
-                return data
-            else:
-                return None
+        # if (current_line == None):
+        #     try:
+        #         # Try to skip blank lines
+        #         while not current_line:
+        #             current_line = bytes(next(scriptgen)[:-1], "utf-8")
+        #     except StopIteration:
+        #         end_of_file = True
 
-        if (current_line == None):
-            try:
-                # Try to skip blank lines
-                while not current_line:
-                    current_line = bytes(next(scriptgen)[:-1], "utf-8")
-            except StopIteration:
-                end_of_file = True
+        # if (not end_of_file):
+        #     x = len(data) + random.randint(0, 3)
+        #     ret_data = current_line[:x]
+        #     current_line = current_line[x:]
+        #     data = ret_data
 
-        if (not end_of_file):
-            x = len(data) + random.randint(0, 3)
-            ret_data = current_line[:x]
-            current_line = current_line[x:]
-            data = ret_data
+        #     if not data:
+        #         data = None
 
-            if not data:
-                data = None
+        #     # Reset the line for the next iteration
+        #     if (len(current_line) == 0):
+        #         current_line = None
+        #         waiting_for_enter = True
 
-            # Reset the line for the next iteration
-            if (len(current_line) == 0):
-                current_line = None
-                waiting_for_enter = True
-
-        return data
+        # return data
 
     return read_file_or_input
 
